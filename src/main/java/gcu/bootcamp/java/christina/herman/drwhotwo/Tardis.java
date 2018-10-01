060901b8ce8baa2085b0e710573ace583af80d64
Bean <bean@Beans-MacBook-Air.local>
2018-10-01 16:04:31 -0700

    Iridian Commit

--- src/main/java/gcu/bootcamp/java/christina/herman/drwhotwo/Tardis.java
+++ src/main/java/gcu/bootcamp/java/christina/herman/drwhotwo/Tardis.java
@@ -5,50 +5,68 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * create class Tardis inheriting DrsFriend
+ * create ArrayList for real friends and enemies
+ * import list and array list
+ */
 public class Tardis extends DrsFriend {
+	//create scanner object
 	private Scanner scanner = new Scanner(System.in);
 	List<String> realFriends = new ArrayList<String>();
 	List<String> enemies = new ArrayList<String>();
 
-
+	// create attributes with access modifiers and types
 	private int whatYear; 
-	private int isOnFire; 
 	private int DrWho; 
 	
-
+	/**
+	 * create constructor for class pass in String name 
+	 * @param calling class being extended 
+	 */
 	public Tardis(String name) {
 		super(name);
 	}
-
-	
+	/**
+	 * create method to get attribute What year
+	 * @return value of what year 
+	 */
 	public int getWhatYear() {
 		return whatYear;
 	}
+	/**
+	 * create method to set attribute what year to pass int value 
+	 * @param set whatYear to value of what year 
+	 */
 	public void setWhatYear(int whatYear) {
 		this.whatYear = whatYear;
 	}
-	public int getIsOnFire() {
-		return isOnFire;
-	}
-	public void setIsOnFire(int isOnFire) {
-		this.isOnFire = isOnFire;
-	}
+	/**
+	 * create method to get attribute DrWho
+	 * @return DrWho value 
+	 */
 	public int getDrWho() {
 		return DrWho;
 	}
-
+	/**
+	 * create method to set attribute DrWho to pass in int value
+	 * @param set drWho to value of DrWho
+	 */
 	public void setDrWho(int drWho) {
 		DrWho = drWho;
 		//This is a test line 
 	}
-
+	// create method createListOfFriends and set value of String 
 	private void createListOfFriends(){
 		realFriends.add("Amy");
 		realFriends.add("Rory");
 		realFriends.add("Tardis");
 	}
-
+	/**
+	 * create method findTheYear 
+	 * @param pass value doctorNum
+	 * @return year value
+	 */
 	public int findTheYear(int doctorNum) {
 		switch (doctorNum) {
 		case 1:
@@ -145,33 +163,50 @@ public class Tardis extends DrsFriend {
 		 
 		}
 	}
-	@Override
-	public boolean isFriendOrFoe(String friend) {
-	
-			if(realFriends.contains(friend)) {
-				System.out.println("Yes, they are a friend of the Dr.\nyou are on the following list of friends " 
-						+ realFriends  +".");
-				return true; 
-			}
-			else if (!realFriends.contains(friend)) {
-				System.out.println("Would you like to add " + friend+ " as a friend (1=Yes,\n"
-						+ "any other number = NO)?");
-				int choice = scanner.nextInt();
-				if(choice ==1) {
-					realFriends.add(friend);
-					Collections.sort(realFriends);
-					System.out.println("Yes, they are a friend of the Dr.\nyou are on the following list of friends " 
-							+ realFriends  +".");
-					return true;
-				}
-			else {
-				enemies.add(friend);
-				
-			}
-		
-		}
-		return false;
+	/**
+	 * create method isFriendOrFoe to pass in String friend value
+	 * implement if statement to check if value will be friend or foe
+	 * if friend add to list of friends
+	 * else if enemy add to list of enemies
+	 */
+    @Override
+    public boolean isFriendOrFoe(String friend) {
+    
+            if(realFriends.contains(friend)) {
+                System.out.println("Yes, they are a friend of the Dr.\nyou are on the following list of friends " 
+                        + realFriends  +".");
+                super.setFriendOrFoe(true);
+                boolean isFriend = super.getFriendOrFoe();
+                return isFriend; 
+            }
+            else if (!realFriends.contains(friend)) {
+                System.out.println("Would you like to add " + friend+ " as a friend (1=Yes,\n"
+                        + "any other number = NO)?");
+                int choice = scanner.nextInt();
+                if(choice ==1) {
+                    realFriends.add(friend);
+                    Collections.sort(realFriends);
+                    System.out.println("Yes, they are a friend of the Dr.\nyou are on the following list of friends " 
+                            + realFriends  +".");
+                    super.setFriendOrFoe(true);
+                    boolean isFriend = super.getFriendOrFoe();
+                    return isFriend; 
+    
+                }
+            else {
+                enemies.add(friend);
+                
+            }
+        
+        }
+        
+            super.setFriendOrFoe(false);
+            boolean isNotFriend = super.getFriendOrFoe();
+            return isNotFriend;
 	}
+    // create method to display enemies list, implement if statement if no enemies are 
+    // added print no enemies
+    // if there is an enemy print String message
 	private void displayEnemies() {
 		if(enemies.isEmpty()) {
 			System.out.println("There are no enemies.....");
@@ -181,17 +216,31 @@ public class Tardis extends DrsFriend {
 		}
 		
 	}
+	// create method displayFriends and print message with realFriends value
+	private void displayFriends() {
+	    System.out.println("The following is a list of friends "+ realFriends );
+	  }
+	// create method promptUserFourTimes implement for loop to prompt user to input names 
+	// and if friend or foe 4 times
+	  private void promptUserFourTimes() {
+	    for(int i = 0; i<4; i++){
+	      String name2 = getNames();
+	      isFriendOrFoe(name2);
+	      }
+	  }
+	  /**
+	   * main() method to call object and instances and methods
+	   * create object of class and set String value
+	   * call methods and run program
+	   */
 	public static void main(String [] args) {
 		Tardis friend = new Tardis("T");
 		friend.createListOfFriends();
 		Collections.sort(friend.realFriends);
-	
-		for(int i = 0; i<4; i++){
-		String name2 = friend.getNames();
-		friend.isFriendOrFoe(name2);
-		}
+		friend.promptUserFourTimes();
 		int num = friend.getNumberFromUser();
 		friend.findTheYear(num);
+		friend.displayFriends();
 		friend.displayEnemies();
 		
 	}
