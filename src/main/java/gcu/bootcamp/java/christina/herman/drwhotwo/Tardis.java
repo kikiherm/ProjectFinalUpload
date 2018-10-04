b085f9c6a6d24478e9ba1d13e0f3cbaf3b798909
william palowski <palowski01@gmail.com>
2018-10-04 12:56:59 -0700

    fixed your case statement and added a new child class

--- src/main/java/gcu/bootcamp/java/christina/herman/drwhotwo/Tardis.java
+++ src/main/java/gcu/bootcamp/java/christina/herman/drwhotwo/Tardis.java
@@ -66,101 +66,73 @@ public class Tardis extends DrsFriend {
 	 * @param pass value doctorNum
 	 * @return year value
 	 */
-	public int findTheYear(int doctorNum) {
-		switch (doctorNum) {
-		case 1:
-			doctorNum=1; 
-			DrWho doctor1 = new DrWho(1, 2109, "Hmmm?");
-			doctor1.setYear(1963);
-			int year1 = doctor1.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor1.getCatchPhrase()
-			+ " and the Earth year is "+ year1 + ".");
-			return year1; 
-		case 2:
-			doctorNum=2; 
-			DrWho doctor2 = new DrWho(4, 2438, "When I say ‘run,’ run. … RUN!");
-			doctor2.setYear(1966);
-			int year2 = doctor2.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor2.getCatchPhrase()
-			+ " and the Earth year is "+  year2 + ".");
-			return year2; 
-		case 3:
-			doctorNum=3; 
-			DrWho doctor3 = new DrWho(3, 1353, "Reverse the polarity of the neutron flow");
-			doctor3.setYear(1970);
-			int year3 = doctor3.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor3.getCatchPhrase()
-			+ " and the Earth year is "+  year3 + ".");
-			return year3;
-		case 4:
-			doctorNum= 4; 
-			DrWho doctor4 = new DrWho(3, 3444, "Would you care for a Jelly Baby?");
-			doctor4.setYear(1974);
-			int year4 = doctor4.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor4.getCatchPhrase()
-			+ " and the Earth year is "+ year4+ ".");
-			return year4; 
-		case 5:
-			doctorNum=5; 
-			DrWho doctor5 = new DrWho(3, 2143, "Sorry, must dash!");
-			doctor5.setYear(1982);
-			int year5 = doctor5.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor5.getCatchPhrase()
-			+ " and the Earth year is "+ year5 +".");
-			return year5;
-		case 6:
-			doctorNum=6; 
-			DrWho doctor6 = new DrWho(7, 6594, "Mmm, I wonder … Aha!");
-			doctor6.setYear(1984);
-			int year6 = doctor6.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor6.getCatchPhrase()
-			+ " and the Earth year is "+ year6 +".");
-			return year6; 
-		case 7:
-			doctorNum=7; 
-			DrWho doctor7 = new DrWho(1, 5847, "Not this time…");
-			doctor7.setYear(1987);
-			int year7 = doctor7.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor7.getCatchPhrase()
-			+" and the Earth year is "+ year7+  ".");
-			return year7;
-		case 8:
-			doctorNum=8; 
-			DrWho doctor8 = new DrWho(3, 1294, "Who am I?");
-			doctor8.setYear(1996);
-			int year8 = doctor8.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor8.getCatchPhrase()
-			+ " and the Earth year is "+ year8 +  ".");
-			return year8; 
-		case 9:
-			doctorNum=9; 
-			DrWho doctor9 = new DrWho(1, 1374,"Fantastic");
-			doctor9.setYear(2005);
-			int year9 = doctor9.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor9.getCatchPhrase()
-			+" and the Earth year is "+ year9+  ".");
-			return year9; 
-		case 10:
-			doctorNum=10; 
-			DrWho doctor10 = new DrWho(2, 3478, "Allons-y");
-			doctor10.setYear(2006);
-			int year10 = doctor10.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor10.getCatchPhrase()
-			+ " and the Earth year is "+ year10+  ".");
-			return year10; 
-		case 11:
-			doctorNum=11; 
-			DrWho doctor11 = new DrWho(3, 1375, "Bowties are Cool");
-			doctor11.setYear(2010);
-			int year11 = doctor11.getYear();
-			System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor11.getCatchPhrase()
-			+ " and the Earth year is "+ year11 + ".");
-			return year11; 
-		default: 
-		System.out.println("There are no more Doctors");
-		return 0; 
-		 
-		}
+	public void findTheYear(int doctorNum) {
+		DrWho doctor;
+		doctor = buildDoctor(doctorNum);
+		System.out.println("This is Doctor " + doctorNum+ ". His saying is " +doctor.getCatchPhrase()
+		+ " and the Earth year is "+ doctor.getYear() + ".");
+		
+	}
+		
+		
+// Made changes to the case statements.  We shortend the code on the case statement and passed back an object to be used for 
+// the display.
+	
+		public DrWho buildDoctor(int doctorNum) {
+			switch (doctorNum) {
+			case 1:
+				DrWho doctor1 = new DrWho(1, 2109, "Hmmm?");
+				doctor1.setYear(1963);
+				return doctor1; 
+			case 2:
+				DrWho doctor2 = new DrWho(4, 2438, "When I say ‘run,’ run. … RUN!");
+				doctor2.setYear(1966);
+				return doctor2; 
+			case 3:
+				DrWho doctor3 = new DrWho(3, 1353, "Reverse the polarity of the neutron flow");
+				doctor3.setYear(1970);
+				return doctor3;
+			case 4:
+				DrWho doctor4 = new DrWho(3, 3444, "Would you care for a Jelly Baby?");
+				doctor4.setYear(1974);
+				return doctor4; 
+			case 5:
+				DrWho doctor5 = new DrWho(3, 2143, "Sorry, must dash!");
+				doctor5.setYear(1982);
+				return doctor5;
+			case 6:
+				DrWho doctor6 = new DrWho(7, 6594, "Mmm, I wonder … Aha!");
+				doctor6.setYear(1984);
+				return doctor6; 
+			case 7:
+				DrWho doctor7 = new DrWho(1, 5847, "Not this time…");
+				doctor7.setYear(1987);
+				return doctor7;
+			case 8:
+				DrWho doctor8 = new DrWho(3, 1294, "Who am I?");
+				doctor8.setYear(1996);
+				return doctor8; 
+			case 9:
+				DrWho doctor9 = new DrWho(1, 1374,"Fantastic");
+				doctor9.setYear(2005);
+				return doctor9; 
+			case 10:
+				DrWho doctor10 = new DrWho(2, 3478, "Allons-y");
+				doctor10.setYear(2006);
+				return doctor10; 
+			case 11:
+				DrWho doctor11 = new DrWho(3, 1375, "Bowties are Cool");
+				doctor11.setYear(2010);
+				return doctor11; 
+			default: 
+			System.out.println("There are no more Doctors");
+			return null; 
+			 
+			}
+		
+		
+		
+		
 	}
 	/**
 	 * create method isFriendOrFoe to pass in String friend value
@@ -233,6 +205,7 @@ public class Tardis extends DrsFriend {
 	   * call methods and run program
 	   */
 	public static void main(String [] args) {
+		Torchwood ally = new Torchwood("Jack Harkness");
 		Tardis friend = new Tardis("T");
 		friend.createListOfFriends();
 		Collections.sort(friend.realFriends);
@@ -241,6 +214,11 @@ public class Tardis extends DrsFriend {
 		friend.findTheYear(num);
 		friend.displayFriends();
 		friend.displayEnemies();
+		List<String>friends = ally.friendList();
+		List<String>enemies = ally.enemyList();
+		ally.displayFriendsList(friends);
+		ally.displayEnemiesList(enemies);
+		
 		
 		
 	}
